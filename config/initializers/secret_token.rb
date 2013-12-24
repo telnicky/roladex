c2c88ad91d7fae43d0861b38bdd2c02e9a860e83
Travis Elnicky <travis.elnicky@gmail.com>
2013-12-24 14:09:19 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Roladex::Application.config.secret_key_base = 'a0dc4bd74d67fdee35969e361e5e14c16b3bb4ade0389c34e406b4b5556301670867dd149ff39b13b158afcf757c7a3f27a30cec4a1959ddc24f7042ed71e888'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Roladex::Application.config.session_store :cookie_store, key: '_roladex_session'
